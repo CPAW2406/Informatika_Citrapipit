2745fb31470d40c170870a2dbe5fe8858609db48
CITRA PIPIT ATIN WIBOWO <150568962+CPAW2406@users.noreply.github.com>
2024-09-01 10:38:53 +0700

    Add files via upload

---
+++ python_Citra Pipit.py
@@ -0,0 +1,6 @@
+print("Hello Wordl")
+print("Belajar Python dari Nol")
+nama= "python pemula"
+
+print("hello"); print("world"); ("Tutorial Python Untuk Pemula")
+Citra_Pipit = "python"; Atin_Wibowo= "pemula"
